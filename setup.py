dd268f9eac93650fa0f5392399720474bc1eb44f
Steve Ayers <californiastephen@outlook.com>
2021-04-29 15:48:54 -0700

    v1.0.0

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
-from setuptools import setup
+import setuptools.command.build_py
+from setuptools import setup, find_packages
+from setuptools.command.install import install as _install
 
-with open("README.md", 'r') as f:
-    long_description = f.read()
+#custom post-installation steps go here:
+class Install(_install):
+    def run(self):
+        _install.do_egg_install(self)
+        #nothing else to do
 
 setup(
+    cmdclass={
+        'install': Install,
+    },
     name='averagejoe',
-    packages=['averagejoe'],
+    packages=find_packages(),
     description='averagejoe',
+    author='Steve Ayers, Ph.D.',
+    author_email='steve@sequenccecentral.com',
+    version='1.0',
+    include_package_data = True,
+    package_data={'': []},
     url='https://github.com/sequencecentral/averagejoe.git',
     # git+https://github.com/sequencecentral/averagejoe.git@main#egg=averagejoe
-    author='Steve Ayers',
-    author_email='steve@sequenccecentral.com',
-    install_requires=[],
-    version='1.0',
-    license='',
+    # Needed to actually package something
+    # Needed for dependencies
+    # install_requires=[''],
+    # *strongly* suggested for sharing
+    # The license can be anything you like
+    license='MIT',
     long_description=open('README.md').read(),
+    install_requires=open('requirements.txt').read(),
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
 )
+#to make an egg:
+#python setup.py bdist_egg
